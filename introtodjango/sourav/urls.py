a56a5b30b2bc2a998fe9138f333cfbd02cfe1177
Sourav Majumder <souravmajumderk99@gmail.com>
2023-06-09 18:42:31 +0600

    learned template

--- introtodjango/sourav/urls.py
+++ introtodjango/sourav/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home, name="home view")
+    path("home/", views.home, name="home view"),
+    path("profile/", views.profile, name="profile page")
 ]
--- introtodjango/sourav/views.py
+++ introtodjango/sourav/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-
 def home(request):
-    return HttpResponse("hello world")
+    # return HttpResponse("hello world")
+    return render(request, "sourav/index.html")
+
+
+def profile(request):
+    return render(request, "sourav/profile.html")
